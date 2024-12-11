28496836191704441904b70c9c164d4bef5841f0
Do-heewan <noh3152@naver.com>
2024-12-11 14:09:51 +0900

    14주차 챌린지문제

--- challenge/Week14_challenge.java
+++ challenge/Week14_challenge.java
@@ -7,25 +7,27 @@ public class Week14_challenge {
         Connection conn;
         Statement stmt = null;
 
+        // 드라이버 설정 변수 선언
         String url = "jdbc:mysql://localhost:3306/bookdb_java";
         String user = "root";
         String password = "0427";
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver"); // jdbc 드라이버 생성
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("DB연결 완료");
             stmt = conn.createStatement();
-            stmt.executeQuery("UPDATE book SET author = '제인 오스틴' WHERE id = 2");
+            stmt.executeUpdate("UPDATE book SET title = 'Pride & Prejudice', author = '제인 오스틴' WHERE id = 2"); // SQL문 작성
 
             printTable(stmt);
         } catch (ClassNotFoundException e) {
-            System.out.println(e + "JDBC 드라이버 로드 에러");
+            System.out.println("JDBC 드라이버 로드 에러");
         } catch (SQLException e) {
-            System.out.println("SQL 실행 에러");
+            System.out.println(e + "SQL 실행 에러");
         }
     }
 
+    // SQL문 출력 메소드
     private static void printTable(Statement stmt) {
         ResultSet srs;
         System.out.printf("%4s|%-30s|%-30s|%-10s\n", "id", "title", "publisher", "author");
